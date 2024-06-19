da6646a577fcb58a533fe4bb6d6afbc5b4bc2450
MessaGE1231 <kostiv049@gmail.com>
2024-06-20 01:04:43 +0300

    first commit

---
+++ my-project/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<button id="btn" value="{val}" on:click={check} class="border-black border-solid border-2 w-4 h-4 rounded-full "></button>
+
+
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.gray.100);
+}
+</style>
+
+<script>
+import "../routes/style.css";
+
+    $: val = false
+    
+function check () {
+    val == false ? val = true : val = false 
+    val == false ? btn.style.background = "white" : btn.style.background = "lightblue"
+}
+</script>
---
+++ my-project/svelte.config.js
@@ -0,0 +1,24 @@
+// import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
